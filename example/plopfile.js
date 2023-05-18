847be0662c7e3bbde1139ec9a8f2538fcd11d3a2
welkin <welkin105380@163.com>
2023-05-18 16:25:33 +0800

    chore: 添加 plop，快速生成 page

---
+++ example/plopfile.js
@@ -0,0 +1,32 @@
+export default function (plop) {
+  plop.setHelper('formatDate', function () {
+    const today = new Date()
+    const year = today.getFullYear()
+    const month = today.getMonth() + 1 > 10 ? today.getMonth() + 1 : '0' + (today.getMonth() + 1)
+
+    const day = today.getDate() > 10 ? today.getDate() : '0' + today.getDate()
+    return `${year}.${month}.${day}`
+  })
+  // page generator
+  plop.setGenerator('page', {
+    description: 'auto create page',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'page name please'
+      }
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: 'src/views/articles/{{dashCase name}}/index.vue',
+        templateFile: 'plop-templates/page.hbs'
+      },
+      {
+        type: 'add',
+        path: 'src/views/articles/{{dashCase name}}/md/index.md'
+      }
+    ]
+  })
+}
